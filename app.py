b6a60ccac1e300c8b3e99be2d29de4d6fd29216d
Sushma-Goel <78504943+Sushma-Goel@users.noreply.github.com>
2021-02-16 14:28:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ def predict():
         elif(Fuel_Type_Petrol=='Diesel'):
                 Fuel_Type_Petrol=0
                 Fuel_Type_Diesel=1
-                    else:
-                    Fuel_Type_Petrol=0
-                    Fuel_Type_Diesel=0
+        else:
+                Fuel_Type_Petrol=0
+                Fuel_Type_Diesel=0
         Year=2020-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
